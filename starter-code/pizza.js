874b9844fc561dde666de34ab77d8e745ce60bc1
javidurani <jduraniglesias21122@columbushs.com>
2021-01-07 09:02:43 -0800

    iteration 4 missing 3 unknown

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -92,8 +92,36 @@ function renderGlutenFreeCrust() {
 
 function renderButtons() {
   // Iteration 3: add/remove the class "active" of each `<button class="btn">`
-  let button = document.getElementsByClassName('btn');
-
+  if(state.pepperoni){
+    document.querySelector('.btn-pepperoni').classList.add("active")
+  }
+  else{
+    document.querySelector('.btn-pepperoni').classList.remove("active")
+  }
+  if(state.mushrooms){
+    document.querySelector('.btn-mushrooms').classList.add("active")
+  }
+  else{
+    document.querySelector('.btn-mushrooms').classList.remove("active") 
+  }
+  if(state.greenPeppers){
+    document.querySelector('.btn-green-peppers').classList.add("active")
+  }
+  else{
+    document.querySelector('.btn-green-peppers').classList.remove("active") 
+  }
+  if(state.whiteSauce){
+    document.querySelector('.btn-sauce').classList.add("active")
+  }
+  else{
+    document.querySelector('.btn-sauce').classList.remove("active") 
+  }
+  if(state.glutenFreeCrust){
+    document.querySelector('.btn-crust').classList.add("active")
+  }
+  else{
+    document.querySelector('.btn-crust').classList.remove("active") 
+  }
 }
 
 function renderPrice() {
@@ -103,7 +131,7 @@ function renderPrice() {
 }
 
 
-renderEverything()
+
 
 // Iteration 1: Example of a click event listener on `<button class="btn btn-pepperonni">`
 document.querySelector('.btn.btn-pepperonni').onclick = function() {
@@ -134,3 +162,4 @@ document.querySelector('.btn.btn-crust').onclick = function() {
   state.glutenFreeCrust = !state.glutenFreeCrust
   renderEverything()
 }
+renderEverything()
